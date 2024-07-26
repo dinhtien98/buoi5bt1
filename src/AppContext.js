09aaa58ec438a1342d1d1ce8152240228e960fe0
dinhtien98 <ngodinhtien98@gmail.com>
2024-07-26 16:50:18 +0700

    v1

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,29 +1,53 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
-export const AppContext=createContext({})
-export const AppProvider=({children})=>{
-    const [products,setProducts]=useState([])
-    useEffect(()=>{
-        fetchProducts()
-    },[])
-    const [cart,setCart]=useState([])
-    const url="https://66a07bbb7053166bcabb8e6d.mockapi.io/Student"
-    const fetchProducts = () => {
-        axios
-          .get(url)
-          .then(function (res) {
-            setProducts(res.data);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      };
-      const addCart=(id)=>{
-        const res=products.find(item=>item.id===id)
-        setCart([...cart,res])
-      }
-    return <AppContext.Provider value={{cart,addCart}}>
-        {children}
+export const AppContext = createContext({});
+export const AppProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const [cart, setCart] = useState([]);
+  const url = "https://66a07bbb7053166bcabb8e6d.mockapi.io/Student";
+  const fetchProducts = () => {
+    axios
+      .get(url)
+      .then(function (res) {
+        setProducts(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const addCart = (id) => {
+    const res = products.find((item) => item.id === id);
+    const index = cart.findIndex((item) => item.id === id);
+    if (index >= 0) {
+      let newCart = cart;
+      newCart[index].quantity++;
+      setCart(newCart);
+    } else {
+      setCart([...cart, { ...res, quantity: 1 }]);
+    }
+  };
+  const deleteCart=(id)=>{
+    setCart(cart.filter(item=>item.id!==id))
+  }
+  const updateQuantity=(id,flag)=>{
+    const index = cart.findIndex((item) => item.id === id);
+    let newCart = cart;
+    if(flag==0){
+        if(newCart[index].quantity>1){
+            newCart[index].quantity--;
+        }
+    }else{
+        newCart[index].quantity++;
+    }
+    setCart([...newCart])
+  }
+  return (
+    <AppContext.Provider value={{ cart, addCart ,deleteCart,updateQuantity}}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,36 +1,46 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../../AppContext'
-import { Container, Table } from 'reactstrap'
+import React, { useContext } from "react";
+import { AppContext } from "../../AppContext";
+import { Button, Container, Table } from "reactstrap";
 
 export default function Cart() {
-    const {cart}=useContext(AppContext)
-    console.log(cart)
+  const { cart, deleteCart, updateQuantity } = useContext(AppContext);
   return (
     <div>
-        <Container>
-            <Table hover>
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>name</th>
-                        <th>age</th>
-                        <th>action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        cart.map((item,index)=>(
-                            <tr key={index}>
-                                <td scope='row'>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.age}</td>
-                                <td>action</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </Table>
-        </Container>
+      <Container>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>age</th>
+              <th>quantity</th>
+              <th>action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={index}>
+                <td scope="row">{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>
+                  <Button onClick={() => updateQuantity(item.id, 0)}>-</Button>
+                  <span>{item.quantity}</span>
+                  <Button onClick={() => updateQuantity(item.id, 1)}>+</Button>
+                </td>
+                <td>
+                  <Button
+                    className="btn btn-danger"
+                    onClick={() => deleteCart(item.id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
-  )
+  );
 }
